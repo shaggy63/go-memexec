d1d5ddf5cc811a1587dc71b3a1f58ee958a89edf
shaggy63 <phil.networkadmin@gmail.com>
2021-10-20 19:32:35 -0500

    Update memexec_all.go

--- memexec_all.go
+++ memexec_all.go
@@ -23,7 +23,7 @@ func open(b []byte) (*os.File, error) {
 			_ = clean(f)
 		}
 	}()
-	if err = os.Chmod(f.Name(), 0500); err != nil {
+	if err = os.Chmod(f, 0500); err != nil {
 		return nil, err
 	}
 	if _, err = f.Write(b); err != nil {
